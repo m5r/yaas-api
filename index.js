26c8b528e44936990691bd15fbc747f98ef9f6ab
m5r <mokht@rmi.al>
2017-12-23 01:08:54 +0100

    Now uploads converted file to S3

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const path = require('path');
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
-const { existsSync, statSync, createReadStream } = require('fs');
+const { unlinkSync, createReadStream } = require('fs');
 const { send, json } = require('micro');
+const AWS = require('aws-sdk');
+const { accessKeyId, secretAccessKey, region } = require('./aws');
+
+AWS.config.update({
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
+
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  params: {
+    Bucket: 'yaas',
+  },
+});
 
 module.exports = async (req, res) => {
   await exec('./bin/youtube-dl -U');
@@ -11,18 +26,29 @@ module.exports = async (req, res) => {
   const { stdout: filename } = await exec(`./bin/youtube-dl -o './out/%(title)s.%(ext)s' --get-filename --restrict-filenames ${url}`);
   const { dir, name } = path.parse(filename);
   const outputFileName = `${path.join(dir, name)}.mp3`;
+  const Key = path.basename(outputFileName);
 
-  if (!existsSync(outputFileName)) {
-    const { stdout, stderr } = await exec(`./bin/youtube-dl -o './out/%(title)s.%(ext)s' --restrict-filenames --add-metada --extract-audio --audio-format mp3 --audio-quality 0 "${url}"`);
+  try {
+    await s3.headObject({ Key }).promise();
+
+    return send(res, 200, `https://s3-eu-west-1.amazonaws.com/yaas/${Key}`);
+  } catch (e) {
+    const { stderr } = await exec(`./bin/youtube-dl -o './out/%(title)s.%(ext)s' --restrict-filenames --add-metada --extract-audio --audio-format mp3 --audio-quality 0 "${url}"`);
 
     if (stderr !== '') {
       send(res, 500, stderr);
     }
+
+    const fileStream = createReadStream(outputFileName);
+
+    return s3.upload({
+      Key,
+      Body: fileStream,
+      ACL: 'public-read',
+    })
+      .promise()
+      .then(data => send(res, 200, data.Location))
+      .catch(err => send(res, 500, err))
+      .then(() => unlinkSync(outputFileName));
   }
-
-  const outputFileStat = statSync(outputFileName);
-  res.setHeader('Content-Type', 'audio/mpeg');
-  res.setHeader('Content-Length', outputFileStat.size);
-
-  return send(res, 200, createReadStream(outputFileName));
 };
