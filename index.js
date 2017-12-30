9abe4b12d17f65f6358f8e06aca8354a4b15f7e7
m5r <me@mokhtarmial.com>
2017-12-30 13:23:09 +0100

    Added redis to cache requests; Doesn't try to update youtube-dl every time it receives a request anymore

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ const { unlinkSync, createReadStream } = require('fs');
 const { send, json } = require('micro');
 const AWS = require('aws-sdk');
 const microCors = require('micro-cors');
+const Redis = require('ioredis');
 const { accessKeyId, secretAccessKey, region } = require('./aws');
 
-const cors = microCors({
-  allowMethods: ['POST'],
-  allowHeaders: ['Accept', 'Content-Type'],
-  origin: 'https://www.yaas.tools',
-});
+const redis = new Redis('redis://redis:6379');
 
 AWS.config.update({
   region,
@@ -26,14 +23,28 @@ const s3 = new AWS.S3({
   },
 });
 
-module.exports = cors(async (req, res) => {
-  await exec('./bin/youtube-dl --no-check-certificate -U');
+const cors = microCors({
+  allowMethods: ['POST'],
+  allowHeaders: ['Accept', 'Content-Type'],
+  origin: 'https://www.yaas.tools',
+});
 
+module.exports = cors(async (req, res) => {
   const { url } = await json(req);
+  const keyFromUrl = await redis.get(url);
+
+  if (keyFromUrl) {
+    return send(res, 200, `https://s3-eu-west-1.amazonaws.com/yaas/${keyFromUrl}`);
+  }
+
   const { stdout: filename } = await exec(`./bin/youtube-dl --no-check-certificate -o './out/%(title)s.%(ext)s' --get-filename --restrict-filenames ${url}`);
   const { dir, name } = path.parse(filename);
   const outputFileName = `${path.join(dir, name)}.mp3`;
   const Key = path.basename(outputFileName);
+
+  if (await redis.get(Key)) {
+    return send(res, 200, `https://s3-eu-west-1.amazonaws.com/yaas/${Key}`);
+  }
 
   try {
     await s3.headObject({ Key }).promise();
@@ -43,16 +54,19 @@ module.exports = cors(async (req, res) => {
     const { stderr } = await exec(`./bin/youtube-dl --no-check-certificate -o './out/%(title)s.%(ext)s' --restrict-filenames --add-metada --extract-audio --audio-format mp3 --audio-quality 0 "${url}"`);
 
     if (stderr !== '') {
-      send(res, 500, stderr);
+      console.error(stderr);
+      send(res, 500, 'Error when calling youtube-dl');
     }
 
-    const fileStream = createReadStream(outputFileName);
+    redis.set(Key, url);
+    redis.set(url, Key);
 
+    const fileStream = createReadStream(outputFileName);
     return s3.upload({
       Key,
       Body: fileStream,
       ACL: 'public-read',
-      ContentType : 'audio/mpeg',
+      ContentType: 'audio/mpeg',
       ContentDisposition: 'attachment',
       Tagging: `requester=${(req.headers['x-forwarded-for'] || '').split(',')[0] || req.connection.remoteAddress}`,
       Metadata: {
